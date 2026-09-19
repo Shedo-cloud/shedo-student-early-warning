3b6d813163b0ff88a7c079006ea299908c728b55
Shedo-cloud <anishedrack@gmail.com>
2026-09-19 12:54:35 -0400

    Update Shedo_App.py

--- Shedo_App.py
+++ Shedo_App.py
@@ -7,37 +7,37 @@ st.set_page_config(
 )
 
 dashboard = st.Page(
-    "data/01_Dashboard.py",
+    "01_Dashboard.py",
     title="Dashboard",
     icon="📊"
 )
 
 students = st.Page(
-    "data/02_Student_Profiles.py",
+    "02_Student_Profiles.py",
     title="Student Profiles",
     icon="👨‍🎓"
 )
 
 at_risk = st.Page(
-    "data/03_At_Risk_Students.py",
+    "03_At_Risk_Students.py",
     title="At-Risk Students",
     icon="⚠️"
 )
 
 interventions = st.Page(
-    "data/04_Interventions.py",
+    "04_Interventions.py",
     title="Interventions",
     icon="🛠️"
 )
 
 parent_reports = st.Page(
-    "data/05_Parent_Reports.py",
+    "05_Parent_Reports.py",
     title="Parent Reports",
     icon="📝"
 )
 
 downloads = st.Page(
-    "data/06_Reports_Downloads.py",
+    "06_Reports_Downloads.py",
     title="Reports & Downloads",
     icon="📥"
 )
